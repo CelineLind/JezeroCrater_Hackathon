5004c9c1aae87672ec881edcb3616b698dbcb34b
Celine <celinelindeque@gmail.com>
2022-10-01 13:20:41 +1000

    added text into middle of images

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -14,27 +14,80 @@ function App() {
   const [ImageNum, setImagenum] = useState(0);
   const [URL, setURL] = useState(jezerocrater);
   const [alttext, setAlttext] = useState('image of jezero crater');
-  const [Information, setInfo] = useState('');
+  const [Information, setInfo] = useState(<></>);
 
   let updateImage0 = () => {
     setImagenum(0);
     setURL(jezerocrater)
+    setAlttext('image of jezero crater')
+    setInfo(
+      <div className='Jezero'>
+        <h1>Jezero Crater</h1>
+        <h2>Welcome to Jezero Crater, home of the Perseverance rover.</h2>
+        <p>
+          This crater contains all sorts of cool rocks. Some rocks formed underwater, while others formed from volcanic activity.
+        </p>
+      </div>
+    )
   }
   let updateImage1 = () => {
     setImagenum(1);
     setURL(seitah)
+    setAlttext('image of jezero crater with low-lying seitah unit highlighted')
+    setInfo(
+      <div className='Seitah'>
+        <h1>Seitah</h1>
+        <h2>This is the Seitah unit. It is the oldest rock in Jezero Crater.</h2>
+        <p>
+          When an impact created Jezero Crater, scientists believe magma was waiting underneath the surface. 
+          The impact exposed the magma, and it spread across the crater.
+        </p>
+      </div>
+    )
   }
   let updateImage2 = () => {
     setImagenum(2);
     setURL(delta)
+    setAlttext('image of jezero crater with the river delta highlighted')
+    setInfo(
+      <div className='Delta'>
+        <h1>River Delta</h1>
+        <h2>This is the River Delta. It was formed by water.</h2>
+        <p>
+          The water carried small pieces of rock from up the river and brought it to Jezero Crater. 
+          The crater soon filled with water and became a lake.
+        </p>
+      </div>
+    )
   }
   let updateImage3 = () => {
     setImagenum(3);
     setURL(maaz)
+    setAlttext('image of jezero crater with maaz unit highlighted')
+    setInfo(
+      <div className='Maaz'>
+        <h1>Maaz</h1>
+        <h2>This is the Maaz unit. It formed from lava.</h2>
+        <p>
+          Scientists are still learning about these rocks. These rocks contain a green mineral called 'Olivine' which forms from lava cooling.
+        </p>
+      </div>
+    )
   }
   let updateImage4 = () => {
     setImagenum(4);
     setURL(four)
+    setAlttext('image of jezero crater')
+    setInfo(
+      <div className='Perseverance'>
+        <h1>Perseverance</h1>
+        <h2>The Perseverance rover is currently on Mars exploring this crater.</h2>
+        <p>
+          NASA have sent Perseverance to take photos of and analyse the rocks. 
+          They have also asked Perseverance to collect some rocks. Scientists hope these rocks will be brought back to Earth in the 2030s.
+        </p>
+      </div>
+    )
   }
 
   let incrementImage = () => {
@@ -69,7 +122,7 @@ function App() {
             <Button title={'4'} action={updateImage4} />
           </div>
           <div className='image'>
-            <Image url={URL} alttext={alttext} action={incrementImage}/>
+            <Image url={URL} alttext={alttext} action={incrementImage} information={Information}/>
           </div>
         </div>
       </div>
--- react-site/src/Image.js
+++ react-site/src/Image.js
@@ -1,10 +1,16 @@
 // Image.js
 import React from "react";
+import './App.css';
 
 export default function Image(props) {
-    let { url, alttext, action} = props;
+    let { url, alttext, action, information} = props;
     return (
-        <img src={url} alt={alttext} width="100%" height="auto" onClick={action}/>
+        <div>
+            <img src={url} alt={alttext} width="100%" height="auto" onClick={action}/>
+            <div className="informative-text">
+                {information}
+            </div>
+        </div>
     );
   }
   
